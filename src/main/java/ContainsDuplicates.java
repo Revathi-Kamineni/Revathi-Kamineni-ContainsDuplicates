3d68f4547661771befd43772231ed5db12a244e6
Revathi-Kamineni <revathikamineni77@gmail.com>
2023-02-05 17:38:55 +0000

    ContainsDuplicates

--- src/main/java/ContainsDuplicates.java
+++ src/main/java/ContainsDuplicates.java
@@ -10,7 +10,8 @@ public class ContainsDuplicates {
     public boolean containsDuplicate(int[] nums){
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
-                if (i == j) continue; //same index position, ignore
+                if (i == j) 
+                continue; 
                 if (nums instanceof int[]) {
                     if (nums[i]==nums[j]) {
                         return true;
